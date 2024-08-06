0ca343ed80bdffaa9c09ff7b011a1039611f2934
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2024-08-06 13:15:36 -0400

    Added one new spell card
    I added the spell card, Ghoulden Protection.

---
+++ script/c77777013.lua
@@ -0,0 +1,52 @@
+-- Ghoulden Protection
+-- Coded by FunnyBones777
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--When a "Ghoulden Order" monster(s) is pendulum summoned, make it unable to be destroyed by battle or card effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.indcon)
+	e2:SetTarget(s.indtg)
+	e2:SetOperation(s.indop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x0420}
+function s.cfilter(c,e,tp)
+	return c:IsSetCard(0x0420) and c:IsSummonPlayer(tp) and c:IsSummonType(SUMMON_TYPE_PENDULUM)
+		and (not e or c:IsRelateToEffect(e))
+end
+function s.indcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,nil,tp)
+end
+function s.indtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetCard(eg)
+end
+function s.indop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=eg:Filter(s.cfilter,nil,e,tp)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		--Cannot be destroyed by battle or card effects
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3008)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		tc:RegisterEffect(e2)
+	end
+end
