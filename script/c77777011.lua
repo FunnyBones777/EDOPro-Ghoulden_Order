e91bf44eed9b03bdd82367d64b53fd2e0b15c1ab
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2024-08-05 22:11:29 -0400

    Another batch of Edits and Fixes

--- script/c77777011.lua
+++ script/c77777011.lua
@@ -4,45 +4,47 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Pendulum attributes
 	Pendulum.AddProcedure(c)
-	--Pendulum Effect Activate
+	--Pendulum Effect Activate 1
 	local e1=Effect.CreateEffect(c)
-  	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.pendtg)
-	e1:SetOperation(s.pendop)
 	c:RegisterEffect(e1)
-  	--flip
+	--Pendulum Effect Activate 2
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetTarget(s.drawtg)
-	e2:SetOperation(s.drawop)
+  	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.pendtg)
+	e2:SetOperation(s.pendop)
 	c:RegisterEffect(e2)
-	--Change to face-down
+  	--flip
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_POSITION)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetTarget(s.postg)
-	e3:SetOperation(s.posop)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTarget(s.drawtg)
+	e3:SetOperation(s.drawop)
 	c:RegisterEffect(e3)
+	--Change to face-down
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_POSITION)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetTarget(s.postg)
+	e4:SetOperation(s.posop)
+	c:RegisterEffect(e4)
 end
-s.listed_series={0x420a}
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,1000) end
 	Duel.PayLPCost(tp,1000)
 end
 function s.filter(c)
-	return c:IsSetCard(0x420a) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsMonster(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.pendtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
