bbe21e3fd92962f24128ae960f33e8abe17ab1e8
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2024-08-08 14:16:35 -0400

    Fixed the ritual again!

--- script/c77777014.lua
+++ script/c77777014.lua
@@ -1,17 +1,5 @@
 -- Ghoulden Ritual
--- Coded by FunnyBones777
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.AddProcGreaterCode({c,1,s.extrafil,s.extraop,s.forcedgroup})
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
-	c:RegisterEffect(e1)
-end
-function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetFieldGroup(tp,LOCATION_DECK,0)
-end
-function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
-	return Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
-end
-function s.forcedgroup(c,e,tp)
-	return c:IsLocation(LOCATION_DECK) and c:IsAbleToGrave()
+	Ritual.AddProcGreaterCode(c,1,nil,77777015)
 end
