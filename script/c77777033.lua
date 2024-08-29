2a2fe63c87b54a79b5cc92e1bcc1739f8f907238
Rene Lino Herrera <70409502+FunnyBones777@users.noreply.github.com>
2024-08-29 07:30:25 -0400

    Added 3 new "Nasty Mutt" monsters

---
+++ script/c77777033.lua
@@ -0,0 +1,19 @@
+-- Nasty Mutt Kano
+-- Coded by FunnyBones777
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetCondition(s.atcon)
+	c:RegisterEffect(e1)
+end
+function s.atcon(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>=3
+end
---
+++ script/c77777034.lua
@@ -0,0 +1,21 @@
+-- Nasty Mutt Jaxson
+-- Coded by FunnyBones777
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.con)
+	e1:SetValue(300)
+	c:RegisterEffect(e1)
+end
+function s.con(e)
+	local ph=Duel.GetCurrentPhase()
+	local tp=Duel.GetTurnPlayer()
+	return tp==e:GetHandlerPlayer() and ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
